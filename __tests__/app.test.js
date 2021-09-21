5dec5e66e5d0a04b46103814267531a24f06d457
jatebo <john.tenbosch@gmail.com>
2021-09-21 14:22:14 +0100

    start writing tests for app, touch listen.js and routers files

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,20 @@
-const db = require('../db/connection.js');
-const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
+const db = require("../db/connection.js");
+const testData = require("../db/data/test-data/index.js");
+const { seed } = require("../db/seeds/seed.js");
+const request = require("supertest");
+const app = require("../app.js");
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe("/api", () => {
+  describe("/categories", () => {
+      it('responds with an array of category objects', async () => {
+         await request(app).get('/api/categories').expect(res.status.toBe(200).then(
+            
+         ) 
+      });
+  });
+
+  it("200: Responds with a JSON object describing all the available API endpoints", () => {});
+});
