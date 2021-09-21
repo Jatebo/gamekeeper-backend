3a215fdd5db4997feddcdd9c3154eb1f2738200a
jatebo <john.tenbosch@gmail.com>
2021-09-21 14:21:28 +0100

    complete writing code for seed

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -10,34 +10,25 @@ const {
 
 const seed = async (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
-  // 1. create tables categories, users, reviews, comments
-  const dropTables = async () => {
-    await db.query(
-      `DROP TABLE IF EXISTS categories, reviews, users, comments;`
-    );
-  };
-  const createCategoriesTable = async () => {
-    await db.query(
-      `CREATE TABLE categories (slug VARCHAR(50) PRIMARY KEY NOT NULL, description VARCHAR(200) NOT NULL);`
-    );
-  };
-  const createUsersTable = async () => {
-    await db.query(
-      `CREATE TABLE users (username VARCHAR(50) PRIMARY KEY NOT NULL, avatar_url TEXT, name VARCHAR(50) NOT NULL);`
-    );
-  };
-  const createReviewsTable = async () => {
-    await db.query(
-      `CREATE TABLE reviews (review_id SERIAL PRIMARY KEY, title VARCHAR(100) NOT NULL, review_body TEXT, designer VARCHAR(50) NOT NULL, review_img_url TEXT DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg', votes INT DEFAULT 0, category VARCHAR(50) REFERENCES categories(slug), owner VARCHAR(50) REFERENCES users(username), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP );`
-    );
-  };
-  const createCommentsTable = async () => {
-    await db.query(
-      `CREATE TABLE comments (comment_id SERIAL PRIMARY KEY, author VARCHAR(50) REFERENCES users(username), review_id INT REFERENCES reviews(review_id), votes INT DEFAULT 0, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, body TEXT );`
-    );
-  };
 
-  // 2. insert categoryData, userData, reviewData, commentData
+  await db.query(`DROP TABLE IF EXISTS comments, reviews, users, categories;`);
+
+  await db.query(
+    `CREATE TABLE categories (slug VARCHAR(50) PRIMARY KEY NOT NULL, description VARCHAR(200) NOT NULL);`
+  );
+
+  await db.query(
+    `CREATE TABLE users (username VARCHAR(50) PRIMARY KEY NOT NULL, avatar_url TEXT, name VARCHAR(50) NOT NULL);`
+  );
+
+  await db.query(
+    `CREATE TABLE reviews (review_id SERIAL PRIMARY KEY, title VARCHAR(100) NOT NULL, review_body TEXT, designer VARCHAR(50) NOT NULL, review_img_url TEXT DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg', votes INT DEFAULT 0, category VARCHAR(50) REFERENCES categories(slug), owner VARCHAR(50) REFERENCES users(username), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP );`
+  );
+
+  await db.query(
+    `CREATE TABLE comments (comment_id SERIAL PRIMARY KEY, author VARCHAR(50) REFERENCES users(username), review_id INT REFERENCES reviews(review_id), votes INT DEFAULT 0, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, body TEXT );`
+  );
+  
   const insertCategoryData = async (categoryData) => {
     const formattedCategoryData = formatCategories(categoryData);
     const categoryStr = format(
@@ -74,18 +65,11 @@ const seed = async (data) => {
     await db.query(commentStr);
   };
 
-  const seedPromises = [
-    dropTables,
-    createCategoriesTable,
-    createUsersTable,
-    createReviewsTable,
-    createCommentsTable,
-    insertCategoryData,
-    insertUserData,
-    insertReviewData,
-    insertCommentData,
-  ];
-  await Promise.all(seedPromises);
+  await insertCategoryData(categoryData);
+  await insertUserData(userData);
+  await insertReviewData(reviewData);
+  await insertCommentData(commentData);
+
 };
 
 module.exports = seed;
