03f42ffc1b276204b01bb0c8b0db7013ed5ada08
jatebo <john.tenbosch@gmail.com>
2021-09-23 11:25:41 +0100

    configure heroku settings in app, package.json and listen.js - remove console logs from reviews controller

--- controllers/reviews.controller.js
+++ controllers/reviews.controller.js
@@ -18,8 +18,6 @@ exports.patchReviewVotes = async (req, res, next) => {
   try {
     const { review_id } = req.params;
     const { inc_votes } = req.body;
-    // console.log(review_id);
-    // console.log(inc_votes);
     const updatedReview = await updateVotesByID(review_id, inc_votes);
     res.status(200).send({ updatedReview });
   } catch (err) {
--- listen.js
+++ listen.js
@@ -1,5 +1,8 @@
 const app = require("./app.js");
 
-app.listen(9090, () => {
-  console.log(`Server is listening on port 9090...`);
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Server is listening on port ${PORT}...`);
 });
